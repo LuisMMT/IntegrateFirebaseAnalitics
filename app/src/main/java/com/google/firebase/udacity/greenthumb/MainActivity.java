9d7e1a5d540250e5a75d57ee54991f0d7dabea71
LuisMMT <lmunoz@gmail.com||>
2018-10-29 17:30:20 -0600

    add firebaseRemoteConfig

--- app/src/main/java/com/google/firebase/udacity/greenthumb/MainActivity.java
+++ app/src/main/java/com/google/firebase/udacity/greenthumb/MainActivity.java
@@ -15,9 +15,11 @@
  */
 package com.google.firebase.udacity.greenthumb;
 
+import android.content.ContentValues;
 import android.content.DialogInterface;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
@@ -27,12 +29,23 @@ import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.crashlytics.android.Crashlytics;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
 import com.google.firebase.udacity.greenthumb.data.DbContract.PlantEntry;
 import com.google.firebase.udacity.greenthumb.data.Preferences;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * {@link MainActivity} displays a list of plants to buy.
@@ -45,6 +58,10 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
     private int mRatingChoice = -1;
 
+    private FirebaseRemoteConfig mFirebaseRemoteConfig;
+    private static final String PLANT_DESCRIPTION_KEY = "plant_description";
+    private static final String DEFAULT_PLANT_DESCRIPTIONS_LEVEL = "basic";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,6 +69,7 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
 
         final RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
@@ -69,10 +87,31 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
         //call the line below te cause a crash
         //Any crashes will reported to firebase with crash reporting
-        fatalError();
+       // fatalError();
 
         // call the line below to report a non-fatal crash
        // reportNonFatalError();
+
+        /**
+         * configuracion de firebase Remote config in debu
+         * para ver los cambios publicados inmediatamente
+         *
+         *          Primero, la muestra obtiene una instancia de objeto de Remote Config y habilita el modo de programador, que permite actualizar la memoria caché con frecuencia.
+         */
+        mFirebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
+        FirebaseRemoteConfigSettings configSettings = new FirebaseRemoteConfigSettings.Builder()
+                .setDeveloperModeEnabled(BuildConfig.DEBUG)
+                .build();
+        mFirebaseRemoteConfig.setConfigSettings(configSettings);
+
+        /**
+         * Después, la muestra configura los valores predeterminados en la app desde un archivo XML:
+         */
+        Map<String,Object> defaultConfigMap = new HashMap<>();
+        defaultConfigMap.put(PLANT_DESCRIPTION_KEY,DEFAULT_PLANT_DESCRIPTIONS_LEVEL);
+        mFirebaseRemoteConfig.setDefaults(defaultConfigMap);
+
+        fetchConfig();
     }
 
     @Override
@@ -195,5 +234,73 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     public void methodThatThrows() throws Exception {
         throw new Exception();
     }
+
+
+    /**
+     * configuracion escuchador
+     */
+    private void fetchConfig(){
+        long  cacheExpiration =3600; // 1 hour in seconds
+
+/**
+ * if developer mode is enable reduce cacheExpiration to 0 so that each fecth goes the server.
+ * this should no be used in release builds
+ */
+        if (mFirebaseRemoteConfig.getInfo().getConfigSettings().isDeveloperModeEnabled()) {
+            cacheExpiration =0;
+        }
+        mFirebaseRemoteConfig.fetch(cacheExpiration)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        //make the fetched config available
+                        //via firebase remote config get<type> calls e.g., getlogin , getString
+                        mFirebaseRemoteConfig.activateFetched();
+
+                        //update the plants description based on the retrieved value
+                        // for plant_description
+                        applyRetrievedPlantDescriptionLevel();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        //an error ocurred when fetching the config
+                        // update the plant descriptions based on the retrieved value
+                        // for plant_description
+                        applyRetrievedPlantDescriptionLevel();
+                    }
+                });
+
+    }
+
+    private void applyRetrievedPlantDescriptionLevel() {
+        String plantDescriptionsLevel = mFirebaseRemoteConfig.getString(PLANT_DESCRIPTION_KEY);
+        Log.d("MainActivity", "plant_description = " + plantDescriptionsLevel);
+
+        String[] plantDescription;
+        if (plantDescriptionsLevel.equals(DEFAULT_PLANT_DESCRIPTIONS_LEVEL)){
+            plantDescription = getResources().getStringArray(R.array.plant_descriptions);
+        }else {
+            plantDescription = getResources().getStringArray(R.array.plant_descriptions_advanced);
+        }
+
+        for (int i = 0; i<plantDescription.length; i++){
+            int plantId= i +1;
+            ContentValues values = new ContentValues();
+            values.put(PlantEntry.COLUMN_DESCRIPTION,plantDescription[i]);
+            getContentResolver().update(PlantEntry.CONTENT_URI,
+                    values,
+                    PlantEntry._ID + " =?",
+            new String[] {String.valueOf(plantId)}) ;
+        }
+
+
+
+
+
+
+    }
+
 
 }
